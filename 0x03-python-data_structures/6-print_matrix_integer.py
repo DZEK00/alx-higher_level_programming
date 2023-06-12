c6284744602001f675373ac202c391677dadd22d
DZEK00 <zakarialyazale7@gmail.com>
2023-06-13 00:25:18 +0100

    task5

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+
+def print_matrix_integer(matrix=[[]]):
+    for rows in matrix:
+        for column in rows:
+            print("{:d}".format(column), end=' ')
+        print()
